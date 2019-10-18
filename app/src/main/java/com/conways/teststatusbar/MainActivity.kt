5faa068a3c3cfe637dff0a931f3c551035018d35
宗士力 <zongshili@olasharing.com>
2019-10-18 11:40:31 +0800

    替换资源

--- app/src/main/java/com/conways/teststatusbar/MainActivity.kt
+++ app/src/main/java/com/conways/teststatusbar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.conways.teststatusbar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.conways.statusbar.StatusBar
 import com.conways.statusbar.StatusBarHelper
 import com.conways.statusbar.StatusBarType
@@ -11,11 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        StatusBarHelper.Transparent(this, StatusBarType.Normal, -0x1000000)
         StatusBar(this)
             .apply {
                 type = StatusBarType.Normal
-                isDarkModel=true
+                color=ContextCompat.getColor(this@MainActivity,R.color.colorAccent)
+                isDarkModel=false
             }
             .commit()
     }
